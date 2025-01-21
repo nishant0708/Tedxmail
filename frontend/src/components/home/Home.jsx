4e64578deb4314d2f7ae5541e79b343215c72e2b
Kratik1093 <kratikmandloi03@gmail.com>
2025-01-21 16:23:19 +0530

    setup

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const Home = memo(() => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+});
+
+export default Home;
