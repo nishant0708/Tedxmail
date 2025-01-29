bd51fc96b8fc4a35b62e6324423966ee69511998
Kratik1093 <kratikmandloi03@gmail.com>
2025-01-29 20:10:43 +0530

    pull lera hu

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require("cors"); // Import cors
 const auth = require("./Routes/auth");
 // const userRoutes = require('./routes/userRoutes');
 dotenv.config();
+// require('dotenv').config();
 
 const app = express();
 
@@ -13,8 +14,8 @@ app.use(cors()); // Enable CORS
 app.use(express.json());
 
 // MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const uri = process.env.MONGO_URI;
+mongoose.connect( uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
